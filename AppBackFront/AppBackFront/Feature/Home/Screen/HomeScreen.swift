2e55aafcc1b161b96b1245ff2897b8ef72326ba2
Lucas Neves <lucas.neves12@icloud.com>
2023-09-06 11:22:07 -0300

    Mudanças da aula 137 - Criando a HomeScreen

---
+++ AppBackFront/AppBackFront/Feature/Home/Screen/HomeScreen.swift
@@ -0,0 +1,76 @@
+//
+//  HomeScreen.swift
+//  AppBackFront
+//
+//  Created by Lucas Neves dos santos pompeu on 06/09/23.
+//
+
+import UIKit
+
+class HomeScreen: UIView {
+    
+    lazy var viewBackground: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIColor(red: 26/255, green: 26/255, blue: 1/255, alpha: 1.0)
+        return view
+    }()
+    
+    lazy var logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "vector")
+        return imageView
+    }()
+    
+    lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.clipsToBounds = true
+        searchBar.layer.cornerRadius = 20
+        searchBar.placeholder = "Pesquise pelo nome!"
+        return searchBar
+    }()
+    
+    lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView()
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.backgroundColor = UIColor(red: 26/255, green: 26/255, blue: 1/255, alpha: 1.0)
+        // TO DO: Register
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 24, bottom: 0, right: 24)
+        layout.scrollDirection = .horizontal
+        collectionView.setCollectionViewLayout(layout, animated: false)
+        return collectionView
+    }()
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        // TO DO: Register
+        tableView.backgroundColor = UIColor(red: 26/255, green: 26/255, blue: 1/255, alpha: 1.0)
+        tableView.separatorStyle = .none
+        return tableView
+    }()
+    
+    
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func addViews() {
+        addSubview(viewBackground)
+        viewBackground.addSubview(logoImageView)
+        viewBackground.addSubview(searchBar)
+        viewBackground.addSubview(collectionView)
+        addSubview(tableView)
+    }
+
+}
--- AppBackFront/AppBackFront/Feature/Home/VC/HomeVC.swift
+++ AppBackFront/AppBackFront/Feature/Home/VC/HomeVC.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 class HomeVC: UIViewController {
+    
+    var screen: HomeScreen?
+    
+    override func loadView() {
+        screen = HomeScreen()
+        view = screen
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .red
     }
 }
