f3362ffc73941eb74c44d89a3867ebcbd212d741
Lucas Neves <lucas.neves12@icloud.com>
2023-09-06 10:31:27 -0300

    Criando TabBar - Aula 136

---
+++ AppBackFront/AppBackFront/Feature/Home/HomeVC.swift
@@ -0,0 +1,17 @@
+//
+//  HomeVC.swift
+//  AppBackFront
+//
+//  Created by Lucas Neves dos santos pompeu on 06/09/23.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
---
+++ AppBackFront/AppBackFront/TabBarController/TabBarVC.swift
@@ -0,0 +1,30 @@
+//
+//  TabBarVC.swift
+//  AppBackFront
+//
+//  Created by Lucas Neves dos santos pompeu on 06/09/23.
+//
+
+import UIKit
+
+class TabBarVC: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTabBar()
+
+    }
+    
+    private func setupTabBar() {
+        let home = UINavigationController(rootViewController: HomeVC())
+        setViewControllers([home], animated: false)
+        tabBar.isTranslucent = false
+        tabBar.tintColor = UIColor(red: 130/255, green: 26/255, blue: 201/255, alpha: 1.0)
+        tabBar.backgroundColor = UIColor(red: 26/255, green: 25/255, blue: 1/255, alpha: 1.0)
+        
+        guard let items = tabBar.items else { return }
+        
+        items[0].image = UIImage(systemName: "trash")
+    }
+
+}
